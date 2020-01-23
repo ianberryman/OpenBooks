bbcc2b8849be81a974a6cb000bcc329b10423c24
Ian Berryman <berryman_ian@yahoo.com>
2020-01-23 06:19:24 -0700

    Added initial Spring Boot

---
+++ src/main/java/org/openbooks/openbooksapi/OpenbooksApiApplication.java
@@ -0,0 +1,13 @@
+package org.openbooks.openbooksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openbooks/openbooksapi/OpenbooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openbooks.openbooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
