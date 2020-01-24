a7518a3435f35812e01ae8c849855028c0eaa44a
Ian Berryman <berryman_ian@yahoo.com>
2020-01-24 06:43:41 -0700

    Added some Account API functionality

--- src/main/java/org/openbooks/openbooksapi/OpenBooksApplication.java
+++ src/main/java/org/openbooks/openbooksapi/OpenBooksApplication.java
@@ -1,7 +1,12 @@
 package org.openbooks.openbooksapi;
 
+import org.openbooks.openbooksapi.core.model.ChartOfAccounts;
+import org.openbooks.openbooksapi.core.model.ChartOfAccountsFactory;
+import org.openbooks.openbooksapi.core.model.Journal;
+import org.openbooks.openbooksapi.core.model.JournalFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenBooksApplication {
@@ -10,5 +15,17 @@ public class OpenBooksApplication {
 
 	    SpringApplication.run(OpenBooksApplication.class, args);
 	}
+
+    @Bean
+    public ChartOfAccounts chartOfAccounts(ChartOfAccountsFactory factory) {
+
+        return factory.build();
+    }
+
+    @Bean
+    public Journal journal(JournalFactory factory) {
+
+        return factory.build();
+    }
 
 }
