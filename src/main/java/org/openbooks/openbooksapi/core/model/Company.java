2d59d714d8685bfd5109414a2514751a42913275
Ian Berryman <contactus@coach-tools.com>
2021-04-18 20:49:59 -0600

    RAML spec changes, switching to GraphQL in NodeJS

--- src/main/java/org/openbooks/openbooksapi/core/model/Company.java
+++ src/main/java/org/openbooks/openbooksapi/core/model/Company.java
@@ -16,21 +16,47 @@ public class Company {
     @JsonProperty("name")
     private String companyName;
 
-    @OneToMany(mappedBy = "company")
+    private String taxId;
+
+    private String taxIdType;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "ADDRESS_ID")
+    private Address address;
+
+    private String website;
+
+/*    @OneToMany(mappedBy = "company")
     @JsonIgnore
     private List<Account> accountList;
 
+    @ManyToMany
+    @JoinTable(
+            name = "COMPANY_CUSTOMER",
+            joinColumns = @JoinColumn(name = "COMPANY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CUSTOMER_ID")
+    )
+    private List<Customer> customerList;*/
+
     public void addAccount(Account account) {
-        accountList.add(account);
+        //accountList.add(account);
 
         account.setCompany(this);
     }
 
     public void removeAccount(Account account) {
-        accountList.remove(account);
+        //accountList.remove(account);
 
         account.setCompany(null);
     }
+
+/*    public void addCustomer(Customer customer) {
+        customerList.add(customer);
+    }
+
+    public void removeCustomer(Customer customer) {
+        customerList.remove(customer);
+    }*/
 
     public boolean idIsNull() {
         return id == null;
@@ -52,11 +78,51 @@ public class Company {
         this.companyName = companyName;
     }
 
-    public List<Account> getAccountList() {
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getTaxIdType() {
+        return taxIdType;
+    }
+
+    public void setTaxIdType(String taxIdType) {
+        this.taxIdType = taxIdType;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+/*    public List<Account> getAccountList() {
         return accountList;
     }
 
     public void setAccountList(List<Account> accountList) {
         this.accountList = accountList;
     }
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(List<Customer> customerList) {
+        this.customerList = customerList;
+    }*/
 }
